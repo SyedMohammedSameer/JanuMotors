5ba9de0b1d27e0d6b74513888826f9d9101d3f05
Mohammed Sameer Syed <sdmohammedsameer@gmail.com>
2025-08-04 21:04:49 -0700

    Delete config.ts

--- config.ts
+++
@@ -1,6 +0,0 @@
-// src/config.ts
-
-// IMPORTANT: Replace these with your actual Supabase project URL and anon key.
-// You can find these in your Supabase project's API settings.
-export const supabaseUrl = 'https://cvonhnwoampvqhkiolby.supabase.co';
-export const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2b25obndvYW1wdnFoa2lvbGJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzNDg5ODEsImV4cCI6MjA2OTkyNDk4MX0.VfNmmYfQ4qQNYn5B4bY1h_H92vU364XhsBrVtwApMxI';
